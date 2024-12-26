efc19e98ce4c230fc19d224f044f81836db70a90
chasethesky79 <seshadri.bharath@yahoo.com>
2024-12-26 17:01:16 -0600

    Initial changes

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import BookList from './BooksList';
+
+function App() {
+  return (
+    <div>
+      <h1>Books management</h1>
+      <BookList/>
+    </div>
+  );
+}
+
+export default App;
